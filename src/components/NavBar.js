9b8b02b1f414224693cbc37f6ac71dabbf63fef0
laurenpowers20 <lauren.powers86@yahoo.com>
2023-04-06 15:34:26 -0400

    mobile menu in progress

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,14 @@
 import Link from "next/link";
-
-// import { FaBars, FaTimes, FaGithub, FaLinkedin } from "react-icons/fa";
+import React, { useState } from "react";
+import { FaBars, FaTimes, FaGithub, FaLinkedin } from "react-icons/fa";
 
 export default function NavBar() {
+  // toggle hamburger menu on click
+  const [nav, setNav] = useState(false);
+  const handleClick = () => setNav(!nav);
   return (
     <>
-      <nav className="text-2xl text-neonYellow-500 font-medium z-20 relative ">
+      <nav className="text-2xl text-neonYellow-500 font-medium z-20 relative">
         <ul className="flex gap-10">
           <Link href={"/"}>
             <li className="hover:text-white">Home</li>
@@ -21,6 +24,35 @@ export default function NavBar() {
           </Link>
         </ul>
       </nav>
+      {/* hamburger */}
+      <div
+        onClick={handleClick}
+        className="sm:hidden z-20 absolute text-neonYellow-500 text-2xl hover:text-white "
+      >
+        {!nav ? <FaBars className="z-20" /> : <FaTimes className="z-20" />}
+      </div>
+      {/* mobile menu */}
+      <ul
+        className={
+          !nav
+            ? "hidden"
+            : "absolute z-20 top-0 left-0 w-full h-screen bg-black text-neonYellow-500 flex flex-col justify-center items-center "
+        }
+      >
+        {" "}
+        <Link onClick={handleClick} href={"/"}>
+          <li className="hover:text-white py-6 text-2xl">Home</li>
+        </Link>
+        <Link onClick={handleClick} href={"/projects"}>
+          <li className="hover:text-white py-6 text-2xl">Projects</li>
+        </Link>
+        <Link onClick={handleClick} href={"/contact"}>
+          <li className="hover:text-white py-6 text-2xl">Contact</li>
+        </Link>
+        <Link onClick={handleClick} href={"/about"}>
+          <li className="hover:text-white py-6 text-2xl">About</li>
+        </Link>
+      </ul>
     </>
   );
 }
