613f82a7088a63250ecb4443a8314938ccd24891
laurenpowers20 <lauren.powers86@yahoo.com>
2023-04-06 21:26:05 -0400

    about me card

---
+++ src/components/AboutCard.js
@@ -0,0 +1,72 @@
+import { motion as m } from "framer-motion";
+import { container, item } from "../../animation.js";
+import headshot from "../assets/HeadShot.png";
+import Image from "next/image.js";
+import Link from "next/link";
+import { GrDocumentText } from "react-icons/gr";
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+
+export default function AboutCard() {
+  return (
+    <div class="container mx-auto mb-10 my-10 rounded-lg">
+      <div class="grid grid-cols-1 md:grid-cols-2 h-screen">
+        <div class="max-h-96 md:h-screen">
+          {" "}
+          <m.div
+            className="sm:flex sm:justify-center"
+            animate={{ y: 0 }}
+            initial={{ y: "100%" }}
+            transition={{ delay: 0.8, duration: 1 }}
+          >
+            <Image
+              src={headshot}
+              alt="Picture of Lauren"
+              width={220}
+              height={220}
+              className="rounded-full mx-auto mb-5"
+            />
+          </m.div>
+        </div>
+        <div class="flex bg-neonBlue-500 rounded text-black p-10">
+          <div class="mb-auto mt-auto max-w-lg">
+            <h1 class="text-3xl uppercase">Lauren Powers</h1>
+            <p class="font-semibold mb-5">Software Engineer | Web Developer</p>
+
+            <m.div
+              variants={container}
+              animate="show"
+              className="font-latoBold"
+            >
+              {" "}
+              <p className="p-2">
+                I am an engineer based in New York City. My professional
+                background lies in financial operations, where I spent 12 years
+                at JP Morgan Chase. During my tenure there, I steadily
+                progressed within the firm and eventually became the assistant
+                manager of one of its largest branches in NYC. However, when my
+                time in banking came to an end, I saw an opportunity to pursue
+                my passion for building and hands-on work. I'm one of those rare
+                people who love putting Ikea furniture together and “figuring it
+                all out!” I was inspired by my friends who had completed
+                Software Engineering Immersive programs and were thriving in
+                their roles.{" "}
+              </p>
+              <p className="p-2">
+                {" "}
+                In the summer of 2022, I began teaching myself JavaScript and
+                coding simple projects. In September, I enrolled in General
+                Assembly's Software Engineering Immersive program, where I
+                worked on both front-end and back-end projects every week.
+                Throughout this journey, I've embraced challenges and pushed
+                myself beyond my comfort zone. It's been a wild ride, and I am
+                confident in my abilities and excited to continue learning and
+                growing in my career.
+              </p>
+            </m.div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,72 +1,31 @@
 import { motion as m } from "framer-motion";
-import { container } from "../../animation.js";
-import headshot from "../assets/HeadShot.png";
-import Image from "next/image.js";
+import AboutCard from "@component/components/AboutCard.js";
 
 export default function About(props) {
   return (
-    <m.main
-      animate={{ y: "0%" }}
-      exit={{ opacity: 1 }}
-      initial={{ y: "100%" }}
-      transition={{
-        duration: 1,
-        ease: "easeOut",
-      }}
-      className="text-neonPink-500 absolute top-0 left-0 w-full h-full bg-black lg:px-48 px-10 overflow-y-auto"
-    >
-      <div className="my-48 p-1 columns-1 gap-40">
-        <m.h1
-          animate={{ y: 0 }}
-          initial={{ y: "100%" }}
-          transition={{ delay: 0.5, duration: 1 }}
-          className="lg:text-8xl text-5xl lg:text-left text-center text-neonPink-500"
-        >
-          About{" "}
-        </m.h1>
-        <m.div
-          className="sm:flex sm:justify-center"
-          animate={{ y: 0 }}
-          initial={{ y: "100%" }}
-          transition={{ delay: 0.8, duration: 1 }}
-        >
-          <Image
-            src={headshot}
-            alt="Picture of Lauren"
-            width={200}
-            height={200}
-            className="rounded-full mx-auto my-5 m-5  "
-          />
-        </m.div>
-
-        <m.div
-          variants={container}
-          animate="show"
-          className="font-latoBold lg:text-4xl text-2xl"
-        >
-          {" "}
-          I am an engineer based in New York City. My professional background
-          lies in financial operations, where I spent 12 years at JP Morgan
-          Chase. During my tenure there, I steadily progressed within the firm
-          and eventually became the assistant manager of one of its largest
-          branches in NYC. However, when my time in banking came to an end, I
-          saw an opportunity to pursue my passion for building and hands-on
-          work. I'm one of those rare people who love putting Ikea furniture
-          together and “figuring it all out!” I was inspired by my friends who
-          had completed Software Engineering Immersive programs and were
-          thriving in their roles.{" "}
-          <p>
-            {" "}
-            In the summer of 2022, I began teaching myself JavaScript and coding
-            simple projects. In September, I enrolled in General Assembly's
-            Software Engineering Immersive program, where I worked on both
-            front-end and back-end projects every week. Throughout this journey,
-            I've embraced challenges and pushed myself beyond my comfort zone.
-            It's been a wild ride, and I am confident in my abilities and
-            excited to continue learning and growing in my career.
-          </p>
-        </m.div>
-      </div>
-    </m.main>
+    <>
+      <m.main
+        animate={{ y: "0%" }}
+        exit={{ opacity: 1 }}
+        initial={{ y: "100%" }}
+        transition={{
+          duration: 1,
+          ease: "easeOut",
+        }}
+        className="text-neonPink-500 absolute top-0 left-0 w-full h-full bg-black lg:px-48 px-10 overflow-y-auto col-span-2"
+      >
+        <div className="mt-48">
+          <m.h1
+            animate={{ y: 0 }}
+            initial={{ y: "100%" }}
+            transition={{ delay: 0.5, duration: 1 }}
+            className="lg:text-9xl text-6xl lg:text-left text-left flex"
+          >
+            About{" "}
+          </m.h1> 
+        </div> 
+        <AboutCard />
+      </m.main>
+    </>
   );
 }
