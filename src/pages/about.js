f2cd9fc276e655b38b1dfd676c369f732e596c72
laurenpowers20 <lauren.powers86@yahoo.com>
2023-04-13 08:30:04 -0400

    adjust padding and size

--- src/pages/about.js
+++ src/pages/about.js
@@ -14,12 +14,12 @@ export default function About(props) {
         }}
         className="text-neonPink-500 absolute top-0 left-0 w-full h-full bg-black lg:px-48 px-10 overflow-y-auto col-span-2"
       >
-        <div className="mt-48">
+        <div className="mt-36">
           <m.h1
             animate={{ y: 0 }}
             initial={{ y: "100%" }}
             transition={{ delay: 0.5, duration: 1 }}
-            className="lg:text-9xl text-6xl lg:text-left text-left flex"
+            className="lg:text-8xl text-5xl pb-10 lg:text-left text-left flex"
           >
             About{" "}
           </m.h1>
